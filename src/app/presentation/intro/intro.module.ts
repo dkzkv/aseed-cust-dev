7aadc047942197c2d9a5d829b8ec4a8e1f25fec8
dkzkv <dkzkv@protonmail.com>
2024-08-10 17:48:10 +0300

    sign-up-component

--- src/app/presentation/intro/intro.module.ts
+++ src/app/presentation/intro/intro.module.ts
@@ -3,19 +3,21 @@ import { NgModule } from "@angular/core";
 import { IntroComponent } from "./components/intro-component/intro-component.component";
 import { IntroRoutingModule } from "./intro.routing.module";
 import { IntroDataService } from "./services/intro-data.service";
+import {IntroDomainModule} from "../../domain/intro-domain/intro-domain.module";
 
 
 @NgModule({
   declarations: [
     IntroComponent
   ],
-  imports: [
-    IntroRoutingModule
-  ],
+    imports: [
+        IntroRoutingModule,
+        IntroDomainModule
+    ],
   providers: [
     IntroDataService,
     provideHttpClient(withInterceptorsFromDi()),
-    
+
   ]
 })
 export class IntroModule {
